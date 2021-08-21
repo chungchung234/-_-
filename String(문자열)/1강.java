25f5986e7f542ef665459e5657bd6c1c0f619a54
chung <a8207637@naver.com>
2021-08-21 19:07:20 +0900

    Create 1강.java

---
+++ String(문자열)/1강.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+class Main{
+    public int solution(String str, char t){
+        int answer=0;
+        str= str.toUpperCase();
+        t=Character.toUpperCase(t);
+        System.out.println(str);
+
+
+        return answer;
+    }
+
+public static void main(String[] args){
+    Main T= new Main();
+    Scanner kb = new Scanner(System.in);
+
+    String str=kb.next();
+    char c = kb.next().charAt(0);
+    System.out.print(T.solution(str, c));
+    }
+}
