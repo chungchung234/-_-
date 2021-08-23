e97b20a6b9faaebf5bd17e257c98a4d6706a78e5
chung <a8207637@naver.com>
2021-08-23 19:57:27 +0900

    2일차

--- String(문자열)/3강.java
+++ String(문자열)/3강.java
@@ -4,12 +4,19 @@ import java.util.*;
 class Main{
     public String solution(String str){
         String answer="";
-
+        //함수활용
+        // for(char x : str.toCharArray()){
+        //     if(Character.isLowerCase(x)) answer+=Character.toUpperCase(x);
+        //   	else answer+=Character.toLowerCase(x);
+        // }
+        //ascii 값활용
+        // for(char x : str.toCharArray()){
+        //     if(x>65&&x<92)answer+=(char)(x+32);
+        //     else answer+=(char)(x-32);
+        // }
         for(char x : str.toCharArray()){
-            if(Character.isLowerCase(x)) answer+=Character.toUpperCase(x);
-          	else answer+=Character.toLowerCase(x);
+            answer+=(char)(x^32);
         }
-
         return answer;
     }
 
@@ -18,7 +25,6 @@ public static void main(String[] args){
     Scanner kb = new Scanner(System.in);
 
     String str=kb.next();
-    char c = kb.next().charAt(0);
-    System.out.print(T.solution(str));
+     System.out.print(T.solution(str));
     }
 }
